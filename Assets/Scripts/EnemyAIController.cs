135ff11c97a2db27506f8e68108b36a08aa437c6
Kaleb Schmenk <kschmenk@outlook.com>
2023-02-14 14:38:00 -0500

    More AI States

--- Assets/Scripts/EnemyAIController.cs
+++ Assets/Scripts/EnemyAIController.cs
@@ -10,7 +10,9 @@ public class EnemyAIController : MonoBehaviour
     { 
         Roam,
         Search,
-        Chase
+        LookAround,
+        Chase,
+        CaughtPlayer
     }
 
     private AIState _AIState;
@@ -30,14 +32,24 @@ public class EnemyAIController : MonoBehaviour
 
     [SerializeField] private float _navMeshAgentSprintSpeed = 7.5f;
     [SerializeField] private float _navMeshAgentJogSpeed = 5.0f;
+    [SerializeField] private float _lookAroundSpeed = 35.0f;
+
 
     private float _navMeshSpeed;
 
     private bool _canSeePlayer;
     private bool _IAmWaiting = false;
 
+    private bool _lookingRight = false;
+    private bool _lookingLeft = false;
+    private bool _lookingBackCenter = false;
+    private bool _doneLooking = false;
+
     private Vector3 _lastPosition;
     private Vector3 _lastKnownLocation;
+    private Vector3 _rotationBeforeLookAround;
+    private Vector3 _rotateToRight;
+    private Vector3 _rotateToLeft;
 
     private Renderer _enemyColor;
 
@@ -74,6 +86,11 @@ public class EnemyAIController : MonoBehaviour
                 _enemyColor.material.color = Color.red;
                 Chase();
 
+                if (Vector3.Distance(transform.position, _player.transform.position) <= _navMeshAgent.stoppingDistance + 1.5f)
+                {
+                    _AIState= AIState.CaughtPlayer;
+                }
+
                 if (_canSeePlayer == false) 
                 { 
                     _AIState = AIState.Search; 
@@ -81,7 +98,6 @@ public class EnemyAIController : MonoBehaviour
                 }
                 break;
 
-            // State changed handled in LookRight()
             case AIState.Search:
                 _enemyColor.material.color = Color.magenta;
                 Search();
@@ -90,8 +106,36 @@ public class EnemyAIController : MonoBehaviour
 
                 if (_navMeshAgent.remainingDistance <= _navMeshAgent.stoppingDistance && _canSeePlayer == false)
                 {
+                    // Look around calcs
+                    _rotationBeforeLookAround = transform.rotation.eulerAngles;
+                    _rotateToRight = new Vector3(_rotationBeforeLookAround.x, _rotationBeforeLookAround.y + 65.0f, _rotationBeforeLookAround.z);
+                    _rotateToLeft = new Vector3(_rotationBeforeLookAround.x, _rotationBeforeLookAround.y - 65.0f, _rotationBeforeLookAround.z);
+
+                    _lookingRight = true;
+                    _AIState = AIState.LookAround;
+                }
+                break;
+
+            case AIState.LookAround:
+                _enemyColor.material.color = Color.cyan;
+                LookAround();
+
+
+                if (_canSeePlayer == true) _AIState = AIState.Chase;
+
+                if (_doneLooking == true && _canSeePlayer == false)
+                {
+                    _doneLooking= false;
                     _AIState = AIState.Roam;
                 }
+                break;
+
+            case AIState.CaughtPlayer:
+                _enemyColor.material.color = Color.blue;
+                Debug.Log("Game over! Player caught");
+                CatchPlayer();
+
+                // Player looks toward Guard
                 break;
         }
     }
@@ -133,16 +177,63 @@ public class EnemyAIController : MonoBehaviour
         }
     }
 
+    // Guard Chases Player
     private void Chase()
     {
         _navMeshAgent.SetDestination(_player.transform.position);
         _navMeshAgent.speed = _navMeshAgentSprintSpeed;
     }
 
+    // Guard Searches Players Last Known Position
     private void Search()
     {
         _navMeshAgent.SetDestination(_lastKnownLocation);
         _navMeshAgent.speed = _navMeshAgentJogSpeed;
+    }
+
+    // Guard Looks Around For Player At Last Known Position
+    private void LookAround()
+    {
+        var step = _lookAroundSpeed * Time.deltaTime;
+        if (_lookingRight == true)
+        {
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(_rotateToRight), step);
+            if (transform.rotation == Quaternion.Euler(_rotateToRight))
+            {
+                _lookingRight = false;
+                _lookingLeft = true;
+            }
+        }
+
+        if (_lookingLeft == true) 
+        {
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(_rotateToLeft), step);
+            if (transform.rotation == Quaternion.Euler(_rotateToLeft))
+            {
+                _lookingLeft = false;
+                _lookingBackCenter = true;
+            }
+        }
+
+        if (_lookingBackCenter == true)
+        {
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(_rotationBeforeLookAround), step);
+            if (transform.rotation == Quaternion.Euler(_rotationBeforeLookAround))
+            {
+                _lookingBackCenter = false;
+                _doneLooking = true;
+            }
+        } 
+    }
+
+    // Guard Has Caught The Player
+    private void CatchPlayer()
+    {
+        _navMeshAgent.speed = 0;
+        _navMeshAgent.SetDestination(transform.position);
+        //FIXME!!! REPLACE WITH PLAYER SCRIPT NAME
+        var playerScript = _player.GetComponent<TestingPlayerController>();
+        if (playerScript != null) playerScript._playerCaptured = true;
     }
 
     // FIXME!! Fix up logic
@@ -158,6 +249,7 @@ public class EnemyAIController : MonoBehaviour
         _navMeshAgent.SetDestination(RandomNavMeshLocation());
     }
 
+    // Guard Shoots Raycast At Max Range _proximityRange Towards Player To See If Player Is Close Enough To Be Chased
     private IEnumerator ProximityCheck()
     {
         RaycastHit hit;
@@ -172,6 +264,10 @@ public class EnemyAIController : MonoBehaviour
             {
                 _canSeePlayer = false;
             }
+        }
+        else
+        {
+            _canSeePlayer = false;
         }
 
         yield return new WaitForSeconds(_proxCheckWaitTime);
--- Assets/Scripts/TestingPlayerController.cs
+++ Assets/Scripts/TestingPlayerController.cs
@@ -6,6 +6,8 @@ public class TestingPlayerController : MonoBehaviour
 {
     private CharacterController _char;
     public float speed = 0.02f;
+
+    public bool _playerCaptured = false;
     void Start()
     {
         _char = GetComponent<CharacterController>();
@@ -13,21 +15,29 @@ public class TestingPlayerController : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKey(KeyCode.W))
+        MovePlayer();
+    }
+
+    private void MovePlayer()
+    {
+        if (_playerCaptured == false)
         {
-            _char.Move(Vector3.forward * speed);
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            _char.Move(Vector3.back * speed);
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            _char.Move(Vector3.left * speed);
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            _char.Move(Vector3.right * speed);
+            if (Input.GetKey(KeyCode.W))
+            {
+                _char.Move(Vector3.forward * speed);
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                _char.Move(Vector3.back * speed);
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                _char.Move(Vector3.left * speed);
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                _char.Move(Vector3.right * speed);
+            }
         }
     }
 }
