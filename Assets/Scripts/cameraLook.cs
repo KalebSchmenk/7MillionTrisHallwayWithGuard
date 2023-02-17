13d38c731e94ecc10cb387187efc84f15425cb68
frougs <eeveetrainer51@gmail.com>
2023-02-16 22:51:39 -0500

    movement fix and camera look

---
+++ Assets/Scripts/cameraLook.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class cameraLook : MonoBehaviour
+{
+public PlayerMovement playerControls;
+Vector2 lookDirection;
+private InputAction look;
+public float camSens;
+
+public Transform pLooking;
+
+float camX;
+float camY;
+
+
+private void Awake() {
+    playerControls = new PlayerMovement();
+}
+private void Start() {
+        Cursor.visible = false;
+        UnityEngine.Cursor.lockState = CursorLockMode.Locked;
+}
+
+private void Update() {
+        lookDirection = look.ReadValue<Vector2>();
+        float lookX = lookDirection.x * Time.deltaTime * camSens;
+        float lookY = lookDirection.y * Time.deltaTime * camSens;
+        camY += lookX;
+        camX -=lookY;
+        camX = Mathf.Clamp(camX, -90f, 90);
+
+        transform.rotation = Quaternion.Euler(camX, camY, 0);
+        pLooking.rotation = Quaternion.Euler(0, camY, 0);
+}
+
+
+private void OnEnable() {
+        look = playerControls.Player.Look; 
+        look.Enable();
+}
+
+private void OnDisable() {
+    look.Disable();
+}
+
+}
--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -9,21 +9,33 @@ public class playerController : MonoBehaviour{
 
     [SerializeField] float playerSpeed = 10.0f;
 
-    public Rigidbody rb;
+    Rigidbody rb;
 
     private InputAction move;
+
+    public Transform lookDirection;
+
+    float hozMove;
+    float vertMove;
+
+    Vector3 mDirection;
 
     public bool hasCrown = false;
     private bool _isCaught = false;
     private GameObject _guard;
     private Vector3 _rotateTowards;
 
+    private void Start() {
+        rb = GetComponent<Rigidbody>();  
+        rb.freezeRotation = true;
+    }
     void Awake() {
         playerControls = new PlayerMovement();
-        rb = GetComponent<Rigidbody>();    
+          
     }
     void Update() {
         moveDirection = move.ReadValue<Vector2>();
+ 
 
         if (_isCaught)
         {
@@ -32,14 +44,21 @@ public class playerController : MonoBehaviour{
             // Not working
             transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(_rotateTowards.normalized), step);
         }
+        if(Input.GetKeyDown(KeyCode.Escape)){
+            UnityEditor.EditorApplication.isPlaying = false;
+        }
+
+        mDirection = lookDirection.forward * moveDirection.y + lookDirection.right * moveDirection.x;
     }
     private void OnEnable() {
         move = playerControls.Player.Move;
         move.Enable();
+
     }
 
     private void OnDisable() {
         move.Disable();
+        
     }
 
 
@@ -50,7 +69,9 @@ public class playerController : MonoBehaviour{
             rb.velocity = Vector3.zero;
             return;
         }
-        rb.velocity = new Vector3(moveDirection.x * playerSpeed, rb.velocity.y, moveDirection.y * playerSpeed);
+        rb.AddForce(mDirection.normalized * playerSpeed, ForceMode.Force);
+
+        
     }
 
     public void CaughtPlayer(GameObject _guard)
