d530a0d9f9beaba9156d4bd783c8d13defa6c2a4
Kaleb Schmenk <kschmenk@outlook.com>
2023-02-21 20:28:30 -0500

    Mouse Movement Fix Attempt (Fail)

--- Assets/Scripts/cameraLook.cs
+++ Assets/Scripts/cameraLook.cs
@@ -5,68 +5,70 @@ using UnityEngine.InputSystem;
 
 public class cameraLook : MonoBehaviour
 {
-public PlayerMovement playerControls;
-Vector2 lookDirection;
-private InputAction look;
-public float mouseSens = 25.0f;
-public float controllerSens = 125.0f;
+    public PlayerMovement playerControls;
+    Vector2 lookDirection;
+    private InputAction look;
+    public float mouseSens = 25.0f;
+    public float controllerSens = 125.0f;
 
-private float lookSens;
-
-
-public Transform pLooking;
-private bool controller;
-
-float camX;
-float camY;
+    private float lookSens;
 
 
-private void Awake() {
-       
+    public Transform pLooking;
+    private bool controller;
+
+    float camX;
+    float camY;
 
 
-    playerControls = new PlayerMovement();
-}
-private void Start() {
+    private void Awake() 
+    {
+        playerControls = new PlayerMovement();
+    }
+
+    private void Start() 
+    {
         Cursor.visible = false;
         UnityEngine.Cursor.lockState = CursorLockMode.Locked;
-}
+    }
 
-private void LateUpdate() {
-
-        if(Gamepad.current != null){
-                lookSens = controllerSens;
+    private void Update() 
+    {
+        if(Gamepad.current != null)
+        {
+             lookSens = controllerSens;
         }
 
-       if(Gamepad.current == null){
-                lookSens = mouseSens;
+        if(Gamepad.current == null)
+        {
+             lookSens = mouseSens;
         }
 
-        
+        float lookX = 0;
+        float lookY = 0;
+
         lookDirection = look.ReadValue<Vector2>();
-        float lookX = lookDirection.x * lookSens * Time.deltaTime;
-        float lookY = lookDirection.y * lookSens * Time.deltaTime;
+
+        lookX = lookDirection.x * lookSens * Time.deltaTime;
+        lookY = lookDirection.y * lookSens * Time.deltaTime;
+        
         camY += lookX; 
-        camX -= lookY; 
-        camX = Mathf.Clamp(camX, -90f, 90);
+        camX -= lookY;
 
-        transform.rotation = Quaternion.Euler(camX, camY, 0);
-        pLooking.rotation = Quaternion.Euler(0, camY, 0);
+        camX = Mathf.Clamp(camX, -90, 90);
 
-       
-}
+        pLooking.transform.rotation = Quaternion.Euler(0, camY, 0);
+        transform.rotation = Quaternion.Euler(camX, 0, 0);
+    }
 
-
-private void OnEnable() {
+    private void OnEnable() 
+    {
         look = playerControls.Player.Look; 
         look.Enable();
-}
+    }
 
-private void OnDisable() {
-    look.Disable();
-}
-
-
-
-
+    private void OnDisable() 
+    {
+        look.Disable();
+    }
 }
